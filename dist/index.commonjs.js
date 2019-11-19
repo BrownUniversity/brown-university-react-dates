e2faba6cb3b17deb17f821e7a6975450b1d708e1
Sumner Warren <sumner_warren@brown.edu>
2019-11-19 14:57:45 -0500

    bump version; update dist & changelog

--- dist/index.commonjs.js
+++ dist/index.commonjs.js
@@ -1,4 +1,4 @@
-/*! brown-university-react-dates v1.0.0 */
+/*! brown-university-react-dates v1.0.1 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("prop-types"), require("react"), require("styled-components"), require("moment"), require("react-dates"), require("brown-university-styles"), require("@testing-library/react"));
